9d5f928006b6f2fcbe66af4623a3d5d35898d986
Ethan Weilheimer <ethanweilheimer@Ethans-MacBook-Pro-2.local>
2024-11-25 19:38:26 -0600

    Edits

--- make_gif.py
+++ make_gif.py
@@ -1,12 +1,17 @@
-import imageio
+from PIL import Image
 import os
+from tqdm import tqdm
 
 start = 0
-end = 99
+end = 199
 
 
 images = []
-for i in range(start, end+1):
-    images.append(imageio.imread(f"images/diffusion{i}.png"))
-imageio.mimsave("images/animation.gif", images, duration=0.1)  # 0.1 seconds per frame
+for i in tqdm(range(start, end+1)):
+    images.append(Image.open(f"images/diffusion{i}.png"))
+    
+images[0].save('pillow_imagedraw.gif', 
+               save_all = True, append_images = images[1:], 
+               optimize = True, duration = 0.1)
+    
 
--- test.py
+++ test.py
@@ -4,36 +4,11 @@ from PIL import Image
 import torch
 from diffusers import StableDiffusionControlNetPipeline, ControlNetModel, UniPCMultistepScheduler
 from diffusers.utils import load_image
-
+import sys
 from transformers import AutoImageProcessor, UperNetForSemanticSegmentation
 from tqdm import tqdm
+import os
 
-shape = (1024,1024)
-scale = 100.0
-octaves = 6
-persistence = 0.5
-lacunarity = 2.0
-
-world = np.zeros(shape)
-for i in range(shape[0]):
-    for j in range(shape[1]):
-        world[i][j] = noise.pnoise2(i/scale, 
-                                    j/scale, 
-                                    octaves=octaves, 
-                                    persistence=persistence, 
-                                    lacunarity=lacunarity, 
-                                    repeatx=1024, 
-                                    repeaty=1024, 
-                                    base=0)
-world_norm = (world  - world.min()) / (world.max() - world.min()) * 255
-world_norm = world_norm.astype(np.uint8)
-img = Image.fromarray(world_norm, mode='L')
-img.save('./images/noise.png')
-
-
-blue = [65,105,225]
-green = [34,139,34]
-beach = [238, 214, 175]
 
 def add_color(world):
       b = 0
@@ -53,24 +28,55 @@ def add_color(world):
                         g += 1
       return color_world
 
-color_world = add_color(world)
-color_world = color_world.astype(np.uint8)
-img_color = Image.fromarray(color_world, mode='RGB')
-img_color.save('./images/procedual.png')
+
+if __name__ == "__main__":
+      proj_name = sys.argv[1]
+      os.mkdir(f'./images/{proj_name}')
+      shape = (1024,1024)
+      scale = 100.0
+      octaves = 6
+      persistence = 0.5
+      lacunarity = 2.0
+
+      world = np.zeros(shape)
+      for i in range(shape[0]):
+            for j in range(shape[1]):
+                  world[i][j] = noise.pnoise2(i/scale, 
+                                                j/scale, 
+                                                octaves=octaves, 
+                                                persistence=persistence, 
+                                                lacunarity=lacunarity, 
+                                                repeatx=1024, 
+                                                repeaty=1024, 
+                                                base=0)
+      world_norm = (world  - world.min()) / (world.max() - world.min()) * 255
+      world_norm = world_norm.astype(np.uint8)
+      img = Image.fromarray(world_norm, mode='L')
+      img.save('./images/{proj_name}/noise.png')
+
+
+      blue = [65,105,225]
+      green = [34,139,34]
+      beach = [238, 214, 175]
+
+      color_world = add_color(world)
+      color_world = color_world.astype(np.uint8)
+      img_color = Image.fromarray(color_world, mode='RGB')
+      img_color.save('./images/{proj_name}/procedual.png')
 
 
 
-controlnet = ControlNetModel.from_pretrained(
-"lllyasviel/sd-controlnet-seg",
-cache_dir="./models"
-).to("cuda")
-pipe = StableDiffusionControlNetPipeline.from_pretrained(
-"runwayml/stable-diffusion-v1-5", controlnet=controlnet, safety_checker=None,
-cache_dir="./models"
-).to("cuda")
-pipe.scheduler = UniPCMultistepScheduler.from_config(pipe.scheduler.config)
-pipe.enable_progress_bar = False
-img_in = img
-for i in tqdm(range(200)):
-      img_in = pipe("Create a landscape of ocean, land, and beach using only the colors {blue}, {green}, and {beach} for blue green and yellow and no other colors. Emualte the style of a 8bit game", img_in, num_inference_steps=20, VERBOSE = False).images[0]
-      img_in.save(f'./images/diffusion{i}.png')
+      controlnet = ControlNetModel.from_pretrained(
+      "lllyasviel/sd-controlnet-seg",
+      cache_dir="./models"
+      ).to("cuda")
+      pipe = StableDiffusionControlNetPipeline.from_pretrained(
+      "runwayml/stable-diffusion-v1-5", controlnet=controlnet, safety_checker=None,
+      cache_dir="./models"
+      ).to("cuda")
+      pipe.scheduler = UniPCMultistepScheduler.from_config(pipe.scheduler.config)
+      pipe.enable_progress_bar = False
+      img_in = img
+      for i in tqdm(range(1)):
+            img_in = pipe("Create a landscape of ocean, land, and beach in the style of an 2D video game", img_in, num_inference_steps=20).images[0]
+            img_in.save(f'./images/{proj_name}/diffusion{i}.png')
