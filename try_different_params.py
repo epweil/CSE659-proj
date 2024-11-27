6b712867a73b5c022f05892db8a9dc9db92f941c
Ethan Weilheimer <e.p.weilheimer@osprey.engr.wustl.edu>
2024-11-27 15:21:51 -0600

    Normal

--- try_different_params.py
+++ try_different_params.py
@@ -36,7 +36,7 @@ def add_color(world):
 
 if __name__ == "__main__":
       controlnet = ControlNetModel.from_pretrained(
-                                    "lllyasviel/sd-controlnet-hed",
+                                    "lllyasviel/sd-controlnet-normal",
                                     cache_dir="./models"
                                     ).to("cuda")
       pipe = StableDiffusionControlNetPipeline.from_pretrained(
@@ -50,7 +50,7 @@ if __name__ == "__main__":
                   for octaves in np.linspace(2,10, 4):
                         for lacunarity in np.linspace(1,10, 4):
                               proj_name = f"scale-{scale}-persistence-{persistence}-octaves-{octaves}-lacunarity-{lacunarity}"  
-                              dir = f'./tests_hed/{proj_name}/'  
+                              dir = f'./tests_normal/{proj_name}/'  
                               if( not os.path.exists(dir)):
                                     os.mkdir(dir)
                                     shape = (512,512)
